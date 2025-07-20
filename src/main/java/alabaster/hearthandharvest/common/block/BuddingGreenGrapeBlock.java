e8d78ef093867cd52334da616c4296cff9b67d0e
AlabasterLeking <cjtopher2012@gmail.com>
2025-07-20 14:02:50 -0400

    fix jade reporting grapes as tomatoes

--- src/main/java/alabaster/hearthandharvest/common/block/BuddingGreenGrapeBlock.java
+++ src/main/java/alabaster/hearthandharvest/common/block/BuddingGreenGrapeBlock.java
@@ -1,15 +1,13 @@
 package alabaster.hearthandharvest.common.block;
 
 import alabaster.hearthandharvest.common.registry.HHModBlocks;
+import alabaster.hearthandharvest.common.registry.HHModItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.*;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.state.BlockState;
@@ -19,6 +17,10 @@ public class BuddingGreenGrapeBlock extends BuddingBushBlock implements Bonemeal
 {
     public BuddingGreenGrapeBlock(Properties properties) {
         super(properties);
+    }
+
+    protected ItemLike getBaseSeedId() {
+        return HHModItems.GREEN_GRAPES.get();
     }
 
     @Override
