26f679626c1bf9e1582a7bb94fb977e9cb1c0b3d
AlabasterLeking <cjtopher2012@gmail.com>
2026-04-11 23:40:48 -0400

    Clarity tweaks

--- src/main/java/alabaster/hearthandharvest/common/event/EffectEvents.java
+++ src/main/java/alabaster/hearthandharvest/common/event/EffectEvents.java
@@ -9,11 +9,20 @@ public class EffectEvents {
 
     @SubscribeEvent
     public static void onEffectApplicable(MobEffectEvent.Applicable event) {
-
-        // Cancel blindness if the entity currently has Clarity
-        if (event.getEffectInstance().getEffect().is(MobEffects.BLINDNESS)
+        // Cancel blindness and darkness if the entity currently has Clarity
+        if ((event.getEffectInstance().getEffect().is(MobEffects.BLINDNESS)
+                || event.getEffectInstance().getEffect().is(MobEffects.DARKNESS))
                 && event.getEntity().hasEffect(HHModEffects.CLARITY)) {
             event.setResult(MobEffectEvent.Applicable.Result.DO_NOT_APPLY);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onEffectAdded(MobEffectEvent.Added event) {
+        // When Clarity is applied, strip any active blindness and darkness
+        if (event.getEffectInstance().getEffect().is(HHModEffects.CLARITY)) {
+            event.getEntity().removeEffect(MobEffects.BLINDNESS);
+            event.getEntity().removeEffect(MobEffects.DARKNESS);
         }
     }
 }
